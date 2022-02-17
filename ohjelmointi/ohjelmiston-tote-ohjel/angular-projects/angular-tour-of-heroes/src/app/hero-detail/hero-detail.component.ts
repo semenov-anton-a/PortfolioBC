41acc19869479ed996ec7a92f4790595eeb31c5b
Anton Semenov <semenov.anton.a@gmail.com>
2022-02-17 20:23:41 +0200

    tour-of-heroes #5

--- ohjelmointi/ohjelmiston-tote-ohjel/angular-projects/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ ohjelmointi/ohjelmiston-tote-ohjel/angular-projects/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -6,30 +6,38 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero | undefined;
+    hero: Hero | undefined;
 
-  constructor(
-    private route: ActivatedRoute,
-    private heroService: HeroService,
-    private location: Location
-  ) {}
+    constructor(
+        private route: ActivatedRoute,
+        private heroService: HeroService,
+        private location: Location
+    ) { }
 
-  ngOnInit(): void {
-    this.getHero();
-  }
+    ngOnInit(): void {
+        this.getHero();
+    }
 
-  getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
-  }
+    getHero(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        this.heroService.getHero(id)
+            .subscribe(hero => this.hero = hero);
+    }
 
-  goBack(): void {
-    this.location.back();
-  }
+    goBack(): void {
+        this.location.back();
+    }
+
+    
+    save(): void {
+        if (this.hero) {
+            this.heroService.updateHero(this.hero)
+                .subscribe(() => this.goBack());
+        }
+    }
 }
